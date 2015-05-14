e85dcc50af2afedf614337554a2bd7cc5770404d
EC2 Default User <ec2-user@ip-172-31-5-83.ap-southeast-2.compute.internal>
2015-05-14 10:35:15 +0000

    domain fix

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git clone https://github.com/caevyn/blog.git
 cd ./blog
 mix do deps.get, deps.compile
 mix obelisk build
-aws s3 cp build s3://caevyn-blog/ --recursive
+aws s3 cp build s3://maltmurphy.com/ --recursive

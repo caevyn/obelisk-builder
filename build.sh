673ff46a7397eb76e85bb8adbfa0f6e0b8a084a7
Matt Murphy <caevyn@gmail.com>
2015-05-18 21:30:07 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 git clone https://github.com/caevyn/blog.git
 cd ./blog
 mix do deps.get, deps.compile
-mix obelisk build > obelisk_log 2>&1
-aws s3 sync obelisk_log s3://maltmurphy-logs/obelisk_log
+mix obelisk build
 aws s3 sync build s3://maltmurphy.com/ --delete

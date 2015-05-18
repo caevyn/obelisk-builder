3e5086c273d1ab2ae43915eee64c8e50d74d0771
Matt Murphy <m.murphy@nib.com.au>
2015-05-18 15:45:38 +1000

    get obelisk output into s3

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 git clone https://github.com/caevyn/blog.git
 cd ./blog
 mix do deps.get, deps.compile
-mix obelisk build
-aws s3 cp build s3://maltmurphy.com/ --recursive
+mix obelisk build >> obelisk_log 2>&1
+aws s3 sync obelisk_log s3://maltmurphy-logs/obelisk_log
+aws s3 sync build s3://maltmurphy.com/ --delete

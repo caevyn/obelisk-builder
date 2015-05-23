f21c9d50fafe05c86ef857fe54328b3516a05e83
Matt Murphy <caevyn@gmail.com>
2015-05-23 12:08:14 +1000

    Update build.sh
    try some logging

--- build.sh
+++ build.sh
@@ -3,4 +3,11 @@ git clone https://github.com/caevyn/blog.git
 cd ./blog
 mix do deps.get, deps.compile
 mix obelisk build
-aws s3 sync build s3://maltmurphy.com/ --delete
+touch log.txt
+if [ $? -eq 0 ]
+then
+  aws s3 sync build s3://maltmurphy.com/ --delete >> log.txt
+else
+  echo 'obelisk build failed.' >> log.txt
+  aws s3 sync log.txt s3://maltmurphy-logs/ --delete
+fi
